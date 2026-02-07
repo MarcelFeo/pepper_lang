3d2c29049a3504bdb5c891c764305bc8c2c1f5b2
MarcelFeo <marcelfeo29@gmail.com>
2026-02-07 14:55:56 -0300

    add token class

--- Token.py
+++ Token.py
@@ -23,4 +23,19 @@ class TokenType(Enum):
     LPAREN = "LPAREN"
     RPAREN = "RPAREN"
 
+class Token:
+    def __init__(self, type: TokenType, literal: Any, line_number: int, position: int) -> None:
+        self.type = type
+        self.literal = literal
+        self.line_number = line_number
+        self.position = position
+
+    def __str__(self) -> str:
+        return f"Token[{self.type} : {self.literal} : Line {self.line_number} : Position {self.position}]"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+
 

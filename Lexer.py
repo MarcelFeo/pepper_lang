1f1405372bf5dea51f7c4c444d0e694ced639fe8
MarcelFeo <marcelfeo29@gmail.com>
2026-04-25 15:28:29 -0300

    add: new token comma in Lexer.py

--- Lexer.py
+++ Lexer.py
@@ -128,6 +128,8 @@ class Lexer:
                 tok = self.__new_token(TokenType.COLON, self.current_char)
             case ':':
                 tok = self.__new_token(TokenType.ARROW, self.current_char)
+            case ',':
+                tok = self.__new_token(TokenType.COMMA, self.current_char)
             case ';':
                 tok = self.__new_token(TokenType.SEMICOLON, self.current_char)
             case '(':
